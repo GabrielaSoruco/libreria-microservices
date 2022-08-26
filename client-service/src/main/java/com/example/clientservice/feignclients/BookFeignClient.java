b77e91c96ff6bde100fbe41a8b408f34822d0caf
Gabriela Soruco <soruco.gabi@gmail.com>
2022-08-26 15:31:26 -0300

    communication between microservices fixed

--- client-service/src/main/java/com/example/clientservice/feignclients/BookFeignClient.java
+++ client-service/src/main/java/com/example/clientservice/feignclients/BookFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "book-service")
+@FeignClient(name = "book-service", path = "/books")
 public interface BookFeignClient {
 
     @PostMapping
--- client-service/src/main/java/com/example/clientservice/feignclients/LaptopFeignClient.java
+++ client-service/src/main/java/com/example/clientservice/feignclients/LaptopFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "laptop-service")
+@FeignClient(name = "laptop-service", path = "/laptops")
 public interface LaptopFeignClient {
 
     @PostMapping
