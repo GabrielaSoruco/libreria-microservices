bf6632b17d0528fb360e578548d77717fb6ec06c
Gabriela Soruco <soruco.gabi@gmail.com>
2022-08-26 16:52:50 -0300

    circuit breaker pattern added

--- client-service/src/main/java/com/example/clientservice/controller/ClientController.java
+++ client-service/src/main/java/com/example/clientservice/controller/ClientController.java
@@ -4,8 +4,13 @@ import com.example.clientservice.entity.Client;
 import com.example.clientservice.model.Book;
 import com.example.clientservice.model.Laptop;
 import com.example.clientservice.service.ClientService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -44,28 +49,53 @@ public class ClientController {
         return clientService.deleteClientById(id);
     }
 
+    @CircuitBreaker(name = "laptopCB", fallbackMethod = "fallbackSaveLaptop")
     @PostMapping("/saveLaptop")
-    public Laptop saveLaptopByClient(@RequestBody Laptop laptop){
-        return clientService.saveLaptopByClient(laptop);
+    public ResponseEntity<Laptop> saveLaptopByClient(@RequestBody Laptop laptop){
+        return ResponseEntity.ok(clientService.saveLaptopByClient(laptop));
     }
 
+    @CircuitBreaker(name = "laptopCB", fallbackMethod = "fallbackFindLaptopsByClient")
     @GetMapping("/findLaptopsByClient/{id}")
-    public List<Laptop> findLaptopByClient(@PathVariable Integer id){
-        return clientService.findLaptopByClient(id);
+    public ResponseEntity<List<Laptop>> findLaptopByClient(@PathVariable Integer id){
+        return ResponseEntity.ok(clientService.findLaptopByClient(id));
     }
 
+    @CircuitBreaker(name = "bookCB", fallbackMethod = "fallbackSaveBookByClient")
     @PostMapping("/saveBook")
-    public Book saveBookByClient(@RequestBody Book book){
-        return clientService.saveBookByClient(book);
+    public ResponseEntity<Book> saveBookByClient(@RequestBody Book book){
+        return ResponseEntity.ok(clientService.saveBookByClient(book));
     }
-
+    @CircuitBreaker(name = "bookCB", fallbackMethod = "fallbackFindBookByClient")
     @GetMapping("/findBookByClient/{id}")
-    public List<Book> findBookByClient(@PathVariable Integer id){
-        return clientService.findBookByClient(id);
+    public ResponseEntity<List<Book>> findBookByClient(@PathVariable Integer id){
+        return ResponseEntity.ok(clientService.findBookByClient(id));
     }
 
+    @CircuitBreaker(name = "productsCB", fallbackMethod = "fallbackFindProductsByClient")
     @GetMapping("/findProductsByClient/{id}")
-    public Map<String, Object> findProductsByClient(@PathVariable Integer id){
-        return clientService.findProductsByClient(id);
+    public ResponseEntity<Map<String, Object>> findProductsByClient(@PathVariable Integer id){
+        return ResponseEntity.ok(clientService.findProductsByClient(id));
+    }
+
+    private ResponseEntity<Laptop> fallbackSaveLaptop(@RequestBody Laptop laptop, RuntimeException e){
+        return ResponseEntity.ok(new Laptop());
+    }
+
+    private ResponseEntity<List<Laptop>> fallbackFindLaptopsByClient(@PathVariable Integer id){
+        List<Laptop> laptops = new ArrayList<>();
+        return ResponseEntity.ok(laptops);
+    }
+
+    private ResponseEntity<Book> fallbackSaveBookByClient(@RequestBody Book book){
+        return ResponseEntity.ok(new Book());
+    }
+
+    private ResponseEntity<List<Book>> fallbackFindBookByClient(@PathVariable Integer id){
+        return ResponseEntity.ok(new ArrayList<>());
+    }
+
+    private ResponseEntity<Map<String, Object>> fallbackFindProductsByClient(@PathVariable Integer id){
+        return ResponseEntity.ok(new HashMap<>());
     }
 }
